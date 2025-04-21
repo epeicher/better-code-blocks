7cf4f0f816639a4f3853dcf6387cdcaf4bbbb983
Roberto Aranda <roberto.aranda@automattic.com>
2025-04-21 20:07:30 +0200

    Change label for limit height toggle

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'e98b341820e620d070dd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'db313073502de0370fe6');
